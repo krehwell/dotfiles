0d28acb9a8fa9eb3df67e3d6d7decdefdc8ccb69
krehwell <fahrimuham001@gmail.com>
2021-03-30 04:15:24 +0800

    added fish as default terminal

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+# ----- BEHAVIOUR -----
+alias :q='exit'
+alias sex='nautilus . &'
+alias cmd=cmd.exe
+alias vim=nvim
+alias ls='ls --color'
+alias pacman='sudo pacman'
+alias txxx='tmux kill-server'
