104440564339d39c357aa91406fa7c1c97410e25
krehwell <fahrimuham001@gmail.com>
2021-04-10 17:49:57 +0800

    allow polybar on all monitors

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,11 +2,13 @@
 
 # Terminate already running bar instances
 killall -q polybar
-# If all your bars have ipc enabled, you can also use
-# polybar-msg cmd quit
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
+if type "xrandr"; then
+    for m in $(polybar --list-monitors | cut -d":" -f1); do
+        MONITOR=$m polybar --reload bar &
+    done
+else
+    polybar --reload bar &
+fi
 
 echo "Bars launched..."
