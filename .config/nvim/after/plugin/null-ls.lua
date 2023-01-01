eb9d490c8496929038c0096c4c3c33ca4884740b
krehwell <fahrimuham001@gmail.com>
2023-01-02 03:35:46 +0700

    formatter adj

--- .config/nvim/after/plugin/null-ls.lua
+++ .config/nvim/after/plugin/null-ls.lua
@@ -6,41 +6,25 @@ end
 null_ls.setup({
 	debug = false,
 	sources = {
-		null_ls.builtins.formatting.stylua.with({
-			indent_type = "Spaces",
-		}),
+		-- COMMON
+		null_ls.builtins.formatting.stylua.with({ indent_type = "Spaces" }),
 		null_ls.builtins.code_actions.gitsigns,
 
+		-- GO
 		null_ls.builtins.formatting.goimports,
-		-- null_ls.builtins.formatting.gofmt,
-		null_ls.builtins.formatting.gofumpt.with({
-			space = true,
-		}),
-		null_ls.builtins.formatting.goimports_reviser,
+		null_ls.builtins.formatting.golines,
+		-- null_ls.builtins.formatting.gofumpt.with({ space = true, }),
+		-- null_ls.builtins.formatting.goimports_reviser,
 
+		-- TS
 		null_ls.builtins.formatting.prettierd,
-
 		null_ls.builtins.diagnostics.eslint_d,
 		null_ls.builtins.code_actions.eslint_d,
+		null_ls.builtins.diagnostics.dotenv_linter,
 
-		-- null_ls.builtins.diagnostics.cspell.with({
-		-- 	-- filetypes = { "html", "json", "yaml", "markdown", "*" },
-		-- 	-- extra_filetypes = { "toml" },
-		-- 	-- diagnostic_config = {
-		-- 	-- 	-- see :help vim.diagnostic.config()
-		-- 	-- 	underline = true,
-		-- 	-- 	virtual_text = false,
-		-- 	-- 	signs = true,
-		-- 	-- 	update_in_insert = false,
-		-- 	-- 	severity_sort = true,
-		-- 	-- },
-		-- 	timeout = 10000,
-		-- 	disabled_filetypes = { "lua" },
-		-- 	extra_args = { "-r", ".vim/", "-c", ".vim/cspell.json" },
-		-- }),
+		-- CSpell
+		-- null_ls.builtins.diagnostics.cspell.with({ }),
 		-- null_ls.builtins.completion.spell,
 		-- null_ls.builtins.code_actions.cspell,
-
-		null_ls.builtins.diagnostics.dotenv_linter,
 	},
 })
