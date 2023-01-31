137dbc5a55356ab52a6210ad9e251c306c2cb43b
krehwell <fahrimuham001@gmail.com>
2023-01-31 12:46:11 +0700

    new tmux session, per project now!

--- .config/nvim/after/plugin/git.lua
+++ .config/nvim/after/plugin/git.lua
@@ -28,7 +28,7 @@ gitsigns.setup({
 	current_line_blame_opts = {
 		virt_text = true,
 		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-		delay = 1500,
+		delay = 800,
 		ignore_whitespace = false,
 	},
 	current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
