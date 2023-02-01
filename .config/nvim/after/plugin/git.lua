de01257b522279894a778be3e81c155eebc6ff7b
krehwell <fahrimuham001@gmail.com>
2023-02-01 22:16:07 +0700

    faster blame

--- .config/nvim/after/plugin/git.lua
+++ .config/nvim/after/plugin/git.lua
@@ -28,7 +28,7 @@ gitsigns.setup({
 	current_line_blame_opts = {
 		virt_text = true,
 		virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-		delay = 800,
+		delay = 650,
 		ignore_whitespace = false,
 	},
 	current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
