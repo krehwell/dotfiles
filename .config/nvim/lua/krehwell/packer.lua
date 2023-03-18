00778cadd11918e77938d34eb4fdaf98d7b7f989
krehwell <fahrimuham001@gmail.com>
2023-03-18 15:49:36 +0700

    bring back luasnip

--- .config/nvim/lua/krehwell/packer.lua
+++ .config/nvim/lua/krehwell/packer.lua
@@ -47,12 +47,12 @@ packer.startup(function(use)
       { "hrsh7th/nvim-cmp" },
       { "hrsh7th/cmp-buffer" },
       { "hrsh7th/cmp-path" },
-      -- { "saadparwaiz1/cmp_luasnip" },
+      { "saadparwaiz1/cmp_luasnip" },
       { "hrsh7th/cmp-nvim-lsp" },
       { "hrsh7th/cmp-nvim-lua" },
 
       -- snippets
-      -- { "L3MON4D3/LuaSnip" },
+      { "L3MON4D3/LuaSnip" },
       -- { "rafamadriz/friendly-snippets" },
     },
   })
