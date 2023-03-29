6d4d8374e52a68bda2299e396517b8749d7ffd64
krehwell <fahrimuham001@gmail.com>
2023-03-30 03:13:45 +0700

    wilder option to fuzzy search

--- .config/nvim/after/plugin/wilder.lua
+++ .config/nvim/after/plugin/wilder.lua
@@ -3,6 +3,22 @@ if not present then
 	return
 end
 
+wilder.set_option("pipeline", {
+	wilder.branch(
+		wilder.cmdline_pipeline({
+			language = "python",
+			fuzzy = 2, -- 2 partial fuzzy matching (match does not have to begin with the same first letter)
+		}),
+		wilder.python_search_pipeline({
+			-- can be set to wilder#python_fuzzy_delimiter_pattern() for stricter fuzzy matching
+			pattern = wilder.python_fuzzy_delimiter_pattern(),
+			-- omit to get results in the order they appear in the buffer
+			sorter = wilder.python_difflib_sorter(),
+			engine = "re",
+		})
+	),
+})
+
 wilder.set_option(
 	"renderer",
 	wilder.renderer_mux({
