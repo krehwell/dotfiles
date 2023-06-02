425b35cbc635bab3e42e3ed8dbedf9377348a4d8
krehwell <fahrimuham001@gmail.com>
2023-06-02 23:48:32 +0700

    want to try fogbell

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -21,7 +21,7 @@ vim.opt.fillchars = {
 vim.g.colors_off_a_little = 1
 vim.g.monochrome_italic_comments = 1
 
-local colorscheme = "off"
+local colorscheme = "fogbell"
 local present, _ = pcall(vim.api.nvim_command, "colorscheme " .. colorscheme)
 if not present then
   return print("colorscheme " .. colorscheme .. " not found!")
@@ -37,17 +37,28 @@ if colorscheme == "off" then
           hi! SignColumn guibg=NONE
           " hi! Pmenu guibg=#NONE
           hi! PmenuSel guifg=#ffffff
+          hi clear Todo
+        augroup END
+      ]],
+    false
+  )
+end
+
+if colorscheme == "fogbell" then
+  vim.api.nvim_exec(
+    [[
+        augroup transparent_signs
+          au!
+          hi! Normal guibg=NONE ctermbg=NONE
+          hi! VertSplit guibg=NONE guifg=NONE ctermbg=NONE ctermfg=NONE
+          hi! SignColumn guibg=NONE
           hi! LineNr guibg=NONE
-          hi! GitSignsAdd guibg=NONE
-          hi! GitSignsChange guibg=NONE
-          hi! GitSignsDelete guibg=NONE
         augroup END
 
         augroup index_color
           au!
-          " hi! link TelescopeMatching IncSearch
-          " hi! link TelescopeSelection PMenuSel
-          hi clear Todo
+          hi! link TelescopeMatching DiffAdd
+          hi! link TelescopeSelection PMenuSel
         augroup END
       ]],
     false
--- .config/nvim/lua/krehwell/plugins/init.lua
+++ .config/nvim/lua/krehwell/plugins/init.lua
@@ -1,95 +1,96 @@
 return {
-  -- LSP
-  "jose-elias-alvarez/null-ls.nvim",                      -- formatter, etc
-  { "folke/neodev.nvim",         ft = { "lua", "vim" } }, -- improve lua with vim :')
-  {
-    "VonHeikemen/lsp-zero.nvim",
-    branch = "v2.x",
-    dependencies = {
-      { "neovim/nvim-lspconfig" },
-      {
-        "williamboman/mason.nvim",
-        build = function()
-          pcall(vim.cmd, "MasonUpdate")
-        end,
-        cmd = "Mason",
-      },
-      { "williamboman/mason-lspconfig.nvim" },
-    },
-  },
+	-- LSP
+	"jose-elias-alvarez/null-ls.nvim", -- formatter, etc
+	{ "folke/neodev.nvim", ft = { "lua", "vim" } }, -- improve lua with vim :')
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			{ "neovim/nvim-lspconfig" },
+			{
+				"williamboman/mason.nvim",
+				build = function()
+					pcall(vim.cmd, "MasonUpdate")
+				end,
+				cmd = "Mason",
+			},
+			{ "williamboman/mason-lspconfig.nvim" },
+		},
+	},
 
-  -- GIT
-  { "tpope/vim-fugitive",        event = "VeryLazy" },
-  { "lewis6991/gitsigns.nvim",   event = "CursorMoved" },
-  { "tpope/vim-rhubarb",         event = "CursorMoved" },
+	-- GIT
+	{ "tpope/vim-fugitive", event = "VeryLazy" },
+	{ "lewis6991/gitsigns.nvim", event = "CursorMoved" },
+	{ "tpope/vim-rhubarb", event = "CursorMoved" },
 
-  -- CODE EDITING/NAVIGATIONS
-  { "isomoar/vim-css-to-inline", event = "CursorMoved" }, -- css to jsx inline
-  {
-    "tpope/vim-surround",
-    keys = {
-      { "cs", "<Plug>VSurround", desc = "Change surround", mode = "n" },
-      { "ds", "<Plug>Dsurround", desc = "Delete surround", mode = "n" },
-      { "ys", "<Plug>Ysurround", desc = "Yank surround",   mode = "n" },
-      { "S",  mode = "v" },
-    },
-  },
-  {
-    "tpope/vim-repeat",
-    keys = { { "." } },
-  },
-  {
-    "ojroques/vim-oscyank",
-    branch = "main",
-    keys = {
-      { "<C-c>", ":OSCYankVisual<CR>", desc = "Copy to clipboard", mode = "v", silent = true },
-    },
-    init = function()
-      vim.g.oscyank_term = "default"
-    end,
-  },
-  {
-    "mbbill/undotree",
-    keys = {
-      { "<leader>u", ":UndotreeToggle<CR>:UndotreeFocus<CR>", desc = "Toggle undotree", mode = "n" },
-    },
-  },
-  {
-    "mg979/vim-visual-multi",
-    keys = {
-      { "<C-n>", "<Plug>(VM-Select-Next)", desc = "Select next match", mode = { "n", "v" } },
-    },
-  },
+	-- CODE EDITING/NAVIGATIONS
+	{ "isomoar/vim-css-to-inline", event = "CursorMoved" }, -- css to jsx inline
+	{
+		"tpope/vim-surround",
+		keys = {
+			{ "cs", "<Plug>VSurround", desc = "Change surround", mode = "n" },
+			{ "ds", "<Plug>Dsurround", desc = "Delete surround", mode = "n" },
+			{ "ys", "<Plug>Ysurround", desc = "Yank surround", mode = "n" },
+			{ "S", mode = "v" },
+		},
+	},
+	{
+		"tpope/vim-repeat",
+		keys = { { "." } },
+	},
+	{
+		"ojroques/vim-oscyank",
+		branch = "main",
+		keys = {
+			{ "<C-c>", ":OSCYankVisual<CR>", desc = "Copy to clipboard", mode = "v", silent = true },
+		},
+		init = function()
+			vim.g.oscyank_term = "default"
+		end,
+	},
+	{
+		"mbbill/undotree",
+		keys = {
+			{ "<leader>u", ":UndotreeToggle<CR>:UndotreeFocus<CR>", desc = "Toggle undotree", mode = "n" },
+		},
+	},
+	{
+		"mg979/vim-visual-multi",
+		keys = {
+			{ "<C-n>", "<Plug>(VM-Select-Next)", desc = "Select next match", mode = { "n", "v" } },
+		},
+	},
 
-  -- BEAUTIFY
-  {
-    "folke/todo-comments.nvim",
-    event = { "VeryLazy", "CursorMoved" },
-    opts = {
-      signs = false,
-      highlight = {
-        multiline = false, -- enable multine todo comments
-      },
-    },
-  },
-  {
-    "j-hui/fidget.nvim",
-    opts = {
-      window = { relative = "win", blend = 0, zindex = nil, border = "none" },
-    },
-  },
-  {
-    "brenoprata10/nvim-highlight-colors",
-    event = { "VeryLazy", "CursorMoved" },
-    opts = {
-      render = "background", -- or 'foreground' or 'first_column'
-      enable_named_colors = true,
-      enable_tailwind = true,
-    },
-  },
-  { "pbrisbin/vim-colors-off", priority = 1000, lazy = true },
-  { "morhetz/gruvbox",         priority = 1000, lazy = true },
-  { "archseer/colibri.vim",    priority = 1000, lazy = true },
-  { "fxn/vim-monochrome",      priority = 1000, lazy = true },
-  { "jaredgorski/Mies.vim",    priority = 1000, lazy = true },
+	-- BEAUTIFY
+	{
+		"folke/todo-comments.nvim",
+		event = { "VeryLazy", "CursorMoved" },
+		opts = {
+			signs = false,
+			highlight = {
+				multiline = false, -- enable multine todo comments
+			},
+		},
+	},
+	{
+		"j-hui/fidget.nvim",
+		opts = {
+			window = { relative = "win", blend = 0, zindex = nil, border = "none" },
+		},
+	},
+	{
+		"brenoprata10/nvim-highlight-colors",
+		event = { "VeryLazy", "CursorMoved" },
+		opts = {
+			render = "background", -- or 'foreground' or 'first_column'
+			enable_named_colors = true,
+			enable_tailwind = true,
+		},
+	},
+	{ "pbrisbin/vim-colors-off", priority = 1000, lazy = true },
+	{ "morhetz/gruvbox", priority = 1000, lazy = true },
+	{ "archseer/colibri.vim", priority = 1000, lazy = true },
+	{ "fxn/vim-monochrome", priority = 1000, lazy = true },
+	{ "jaredgorski/Mies.vim", priority = 1000, lazy = true },
+	{ "jaredgorski/fogbell.vim" },
 }
