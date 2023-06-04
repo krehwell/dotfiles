f4cb152ba2d266735938e1cde1f5966223d79e41
krehwell <fahrimuham001@gmail.com>
2023-06-04 10:09:19 +0700

    simplified tmux config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+	fish_vi_key_bindings
+
+    alias :q='exit'
+    alias sex='bash -c \'(open . || explorer.exe . || nautilus .) &\''
+    alias vim=nvim
+    alias tmux='tmux -2'
+    alias txxx='tmux kill-server'
 end
