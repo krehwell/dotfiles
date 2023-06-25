21b3a4537aa8e167e77725fe70086dede80ad313
krehwell <fahrimuham001@gmail.com>
2023-06-26 05:14:12 +0700

    tmux-sessionzer 🥰

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,44 +2,49 @@ local wezterm = require("wezterm")
 
 -- This table will hold the configuration.
 local config = {
-	font = wezterm.font_with_fallback({
-		-- font weight to be a little bold
-		{ family = "Consolas", weight = "Medium" },
-		{ family = "Symbols Nerd Font Mono", scale = 0.8 },
-	}),
-	font_size = 14,
+  font = wezterm.font_with_fallback({
+    -- font weight to be a little bold
+    { family = "Consolas",               weight = "Medium" },
+    { family = "Symbols Nerd Font Mono", scale = 0.8 },
+  }),
+  font_size = 14,
 
-	colors = {
-		background = "#161B1A",
-		foreground = "#B8BBBB", -- #B8BBBB, #A1AFA1, #F1F1F1, #A1AFAF, #BBB8BB
-		cursor_bg = "#c6c6c6",
-		cursor_fg = "#161B1A",
-	},
+  colors = {
+    background = "#161B1A",
+    foreground = "#B8BBBB", -- #B8BBBB, #A1AFA1, #F1F1F1, #A1AFAF, #BBB8BB
+    cursor_bg = "#c6c6c6",
+    cursor_fg = "#161B1A",
+  },
 
-	hide_tab_bar_if_only_one_tab = true,
-	-- exit_behavior = "CloseOnCleanExit",
-	native_macos_fullscreen_mode = true,
+  hide_tab_bar_if_only_one_tab = true,
+  -- exit_behavior = "CloseOnCleanExit",
+  native_macos_fullscreen_mode = true,
 
-	window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
-	window_close_confirmation = "NeverPrompt",
-	initial_rows = 40,
-	initial_cols = 110,
+  window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+  window_close_confirmation = "NeverPrompt",
+  initial_rows = 40,
+  initial_cols = 110,
 
-	keys = {
-		{
-			key = "f",
-			mods = "CTRL|CMD",
-			action = wezterm.action.ToggleFullScreen,
-		},
-		-- disable CTRL + SHIFT + P
-		{
-			key = "P",
-			mods = "CTRL|SHIFT",
-			action = "DisableDefaultAssignment",
-		},
-	},
+  keys = {
+    {
+      key = "f",
+      mods = "CTRL|CMD",
+      action = wezterm.action.ToggleFullScreen,
+    },
+    -- disable CTRL+SHIFT+P since it's being used by vim
+    {
+      key = "P",
+      mods = "CTRL|SHIFT",
+      action = "DisableDefaultAssignment",
+    },
+    {
+      key = "p",
+      mods = "CMD",
+      action = wezterm.action({ SendString = "~/utils/tmux-sessionizer\n" }),
+    },
+  },
 
-	cursor_blink_rate = 500,
+  cursor_blink_rate = 500,
 }
 
 return config
