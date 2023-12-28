dae9879aad9bb1a1df6bb03a6b4f2f2b80edd8cf
krehwell <fahrimuham001@gmail.com>
2023-12-29 05:53:46 +0700

    new fern plugins

--- .config/nvim/lua/krehwell/plugins/fern.lua
+++ .config/nvim/lua/krehwell/plugins/fern.lua
@@ -1,41 +1,44 @@
 return {
-  "lambdalisue/fern.vim",
-  dependencies = {
-    "lambdalisue/fern-git-status.vim",
-    "lambdalisue/nerdfont.vim",
-    "lambdalisue/fern-renderer-nerdfont.vim",
-    "lambdalisue/fern-hijack.vim",
-    "andykog/fern-highlight.vim",
-    "lambdalisue/glyph-palette.vim",
-    "andykog/fern-copynode.vim",
-  },
-  lazy = true,
-  ft = { "fern" },
-  keys = {
-    {
-      "<Plug>(my-fern-toggle)",
-      ":Fern . -drawer -reveal=% -width=27 -wait<CR>",
-      silent = true,
-      desc = "fern toggler func",
-    },
-    {
-      "<C-b>",
-      desc = "Toggle drawer",
-      silent = true,
-    },
-  },
-  config = function()
-    vim.g["fern#hide_cursor"] = 0
-    vim.g["fern#default_hidden"] = 1
-    vim.g["fern#renderer"] = "nerdfont"
-    vim.g["fern#renderer#nerdfont#indent_markers"] = 1
-    -- vim.g["fern#renderer#nerdfont#root_symbol"] = " "
+	"lambdalisue/fern.vim",
+	dependencies = {
+		"lambdalisue/fern-git-status.vim",
+		"lambdalisue/nerdfont.vim",
+		"lambdalisue/fern-renderer-nerdfont.vim",
+		"lambdalisue/fern-hijack.vim",
+		"andykog/fern-highlight.vim",
+		"lambdalisue/glyph-palette.vim",
+		"andykog/fern-copynode.vim",
+		"yuki-yano/fern-preview.vim",
+		"LumaKernel/fern-mapping-reload-all.vim",
+	},
+	lazy = true,
+	ft = { "fern" },
+	keys = {
+		{
+			"<Plug>(my-fern-toggle)",
+			":Fern . -drawer -reveal=% -width=27 -wait<CR>",
+			silent = true,
+			desc = "fern toggler func",
+		},
+		{
+			"<C-b>",
+			desc = "Toggle drawer",
+			silent = true,
+		},
+	},
+	config = function()
+		vim.g["fern#hide_cursor"] = 0
+		vim.g["fern#default_hidden"] = 1
+		vim.g["fern#renderer"] = "nerdfont"
+		vim.g["fern#renderer#nerdfont#indent_markers"] = 1
+		-- vim.g["fern#renderer#nerdfont#root_symbol"] = " "
 
-    -- <C-b> behavior
-    -- if no buffer/window name fern then open fern
-    -- if fern buffer exists but not focus, then move cursor to fern window
-    -- if fern buffer exists and focus, then close fern
-    vim.cmd([[
+		-- FERN TOGGLER
+		-- <C-b> behavior
+		-- if no buffer/window name fern then open fern
+		-- if fern buffer exists but not focus, then move cursor to fern window
+		-- if fern buffer exists and focus, then close fern
+		vim.cmd([[
         nnoremap <silent> <C-B> :call ToggleFernBuffer()<CR>
 
         function! ToggleFernBuffer()
@@ -48,7 +51,23 @@ return {
         endfunction
       ]])
 
-    vim.cmd([[
+		-- FERN PREVIEWER
+		vim.cmd([[
+      function! s:fern_previewer() abort
+        nmap <silent> <buffer> p     <Plug>(fern-action-preview:toggle)
+        nmap <silent> <buffer> <C-p> <Plug>(fern-action-preview:auto:toggle)
+        nmap <silent> <buffer> <C-j> <Plug>(fern-action-preview:scroll:down:half)
+        nmap <silent> <buffer> <C-k> <Plug>(fern-action-preview:scroll:up:half)
+      endfunction
+
+      augroup fern-previewer-settings
+        autocmd!
+        autocmd FileType fern call s:fern_previewer()
+      augroup END
+    ]])
+
+		-- FERN CUSTOM SETS
+		vim.cmd([[
         function! s:init_fern() abort
           setlocal nonu norelativenumber
         endfunction
@@ -60,5 +79,5 @@ return {
           autocmd FileType fern call glyph_palette#apply()
         augroup end
       ]])
-  end,
+	end,
 }
