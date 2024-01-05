ecc323d95fb8d03cea583479c5c5204f5a819bbd
krehwell <fahrimuham001@gmail.com>
2024-01-05 11:46:33 +0700

    conform and nvim-lint

---
+++ .config/nvim/lua/krehwell/plugins/conform.lua
@@ -0,0 +1,42 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				javascript = { "prettierd", "prettier" },
+				typescript = { "prettierd", "prettier" },
+				javascriptreact = { "prettierd", "prettier" },
+				typescriptreact = { "prettierd", "prettier" },
+				svelte = { "prettierd", "prettier" },
+				css = { "prettierd", "prettier" },
+				html = { "prettierd", "prettier" },
+				json = { "prettierd", "prettier" },
+				yaml = { "prettierd", "prettier" },
+				markdown = { "prettierd", "prettier" },
+				graphql = { "prettierd", "prettier" },
+				lua = { "stylua" },
+				go = { "goimports", "gofmt" },
+				python = { "isort", "black" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "gq", function()
+			conform.format({
+				lsp_fallback = false,
+				async = true,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
+	end,
+	keys = {
+		{ "gq", desc = "Format file or range (in visual mode)", mode = { "n", "v" } },
+	},
+}
