31af72c3981c2d4db6631759f176b198fea50404
krehwell <fahrimuham001@gmail.com>
2024-01-21 16:03:27 +0700

    update treesitter settings. works normal back now

--- .config/nvim/lua/krehwell/plugins/treesitter.lua
+++ .config/nvim/lua/krehwell/plugins/treesitter.lua
@@ -4,25 +4,25 @@ return {
 	dependencies = {
 		{ "windwp/nvim-ts-autotag" }, -- html auto tag
 		{ "nvim-treesitter/nvim-treesitter-refactor" }, -- highlight usages, definition, etc
-		{ "JoosepAlviste/nvim-ts-context-commentstring" }, -- for Comment.nvim
 	},
 
 	config = function()
 		require("nvim-treesitter.configs").setup({
-			ensure_installed = { "vimdoc", "typescript", "lua", "go", "bash", "tsx", "javascript", }, -- A list of parser names, or "all"
+			ensure_installed = { "vimdoc", "typescript", "lua", "go", "bash", "tsx", "javascript" }, -- A list of parser names, or "all"
 			sync_install = false, -- Install parsers synchronously (only applied to `ensure_installed`)
 			auto_install = false, -- Automatically install missing parsers when entering buffer
 
 			highlight = {
-				enable = true,
+				enable = false,
 				additional_vim_regex_highlighting = false,
-				use_languagetree = true,
-				-- disable = function(_, bufnr)
-				-- 	local buf_name = vim.api.nvim_buf_get_name(bufnr)
-				-- 	local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
-				-- 	return file_size > 256 * 1024
-				-- end,
+				use_languagetree = false,
+				disable = function(_, bufnr)
+					local buf_name = vim.api.nvim_buf_get_name(bufnr)
+					local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
+					return file_size > 256 * 1024
+				end,
 			},
+
 			incremental_selection = {
 				enable = true,
 				keymaps = {
@@ -32,17 +32,39 @@ return {
 					node_decremental = "<c-backspace>",
 				},
 			},
+
 			indent = { enable = true, disable = { "yaml" } },
+
 			autotag = {
-				enable = true, -- auto close html tag
+				enable = true,
+				enable_rename = true,
+				enable_close = true,
 				enable_close_on_slash = false,
+				filetypes = {
+					"html",
+					"javascript",
+					"typescript",
+					"javascriptreact",
+					"typescriptreact",
+					"svelte",
+					"vue",
+					"tsx",
+					"jsx",
+					"rescript",
+					"xml",
+					"php",
+					"markdown",
+					"astro",
+					"glimmer",
+					"handlebars",
+					"hbs",
+				},
 			},
+
 			refactor = {
 				highlight_definitions = {
 					enable = true,
-					clear_on_cursor_move = false,
-					enable_rename = true,
-					enable_close = true,
+					clear_on_cursor_move = true,
 				},
 			},
 		})
