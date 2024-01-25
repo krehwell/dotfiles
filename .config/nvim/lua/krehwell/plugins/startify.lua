8061865236fc4e833749735d6bafdcc18664fbe1
krehwell <fahrimuham001@gmail.com>
2024-01-25 17:23:19 +0700

    adj

--- .config/nvim/lua/krehwell/plugins/startify.lua
+++ .config/nvim/lua/krehwell/plugins/startify.lua
@@ -17,14 +17,14 @@ return {
       endfunction
 
       let g:startify_lists = [
-        \ { 'type': 'files',     'header': ['   MRU']            },
         \ { 'type': 'dir',       'header': ['   MRU '. getcwd()] },
+        \ { 'type': 'files',     'header': ['   MRU']            },
         \ { 'type': 'sessions',  'header': ['   Sessions']       },
         \ { 'type': 'bookmarks', 'header': ['   Bookmarks']      },
         \ { 'type': function('s:gitModified'),  'header': ['   git modified']},
         \ { 'type': function('s:gitUntracked'), 'header': ['   git untracked']},
         \ { 'type': 'commands',  'header': ['   Commands']       },
-        \ ]
+      \ ]
 
       let g:startify_change_to_dir = 0
 
