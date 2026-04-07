7d4b8c1d999330776766075181cae69c905ba869
krehwell <fahrimuham001@gmail.com>
2026-04-08 06:51:36 +0700

    colors no-distraction

---
+++ .config/nvim/colors/no-distraction.lua
@@ -0,0 +1,114 @@
+-- No-distraction colorscheme: strips syntax colors, keeps structure visible.
+-- Requires Neovim 0.12+
+
+vim.cmd.highlight("clear")
+if vim.fn.exists("syntax_on") then
+	vim.cmd.syntax("reset")
+end
+vim.o.termguicolors = true
+vim.g.colors_name = "no-distraction"
+
+local fg = "NvimLightGrey2"
+
+---@type table<string, vim.api.keyset.highlight>
+local groups = {
+	Normal = { bg = "none" },
+	NormalNC = { link = "Normal" },
+	NormalFloat = { bg = "none" },
+	FloatBorder = { fg = "NvimLightGrey4", update = true },
+
+	-- Keep comments dimmed but distinguishable.
+	Comment = { fg = "NvimDarkGrey4", italic = true, update = true },
+	DiagnosticUnnecessary = { link = "Comment" },
+
+	-- Tabs / splits.
+	TabLineSel = { link = "DiffText" },
+	TabLine = { link = "Conceal" },
+	TabLineFill = { link = "Conceal" },
+	WinSeparator = { link = "Conceal" },
+
+	-- Illuminate.
+	IlluminatedWordText = { link = "MatchParen" },
+	IlluminatedWordRead = { link = "MatchParen" },
+	IlluminatedWordWrite = { link = "MatchParen" },
+}
+
+-- Vim builtin syntax groups to flatten.
+local vim_syntax = {
+	"Module",
+	"Constant",
+	"String",
+	"Character",
+	"Number",
+	"Boolean",
+	"Float",
+	"Operator",
+	"Identifier",
+	"Function",
+	"Label",
+	"Keyword",
+	"Exception",
+	"PreProc",
+	"Include",
+	"Define",
+	"Macro",
+	"Type",
+	"StorageClass",
+	"Structure",
+	"Typedef",
+	"Special",
+}
+
+for _, g in ipairs(vim_syntax) do
+	groups[g] = { fg = fg }
+end
+
+-- Treesitter capture groups to flatten (new @-style).
+local ts_captures = {
+	"@annotation",
+	"@attribute",
+	"@boolean",
+	"@character",
+	"@constructor",
+	"@constant",
+	"@constant.builtin",
+	"@constant.macro",
+	"@exception",
+	"@variable.member",
+	"@number.float",
+	"@function",
+	"@function.macro",
+	"@function.builtin",
+	"@function.method",
+	"@keyword",
+	"@keyword.function",
+	"@keyword.operator",
+	"@label",
+	"@module",
+	"@number",
+	"@operator",
+	"@variable.parameter",
+	"@property",
+	"@punctuation.bracket",
+	"@punctuation.special",
+	"@punctuation.delimiter",
+	"@string",
+	"@string.special.symbol",
+	"@tag",
+	"@tag.delimiter",
+	"@markup",
+	"@markup.heading",
+	"@markup.raw",
+	"@type",
+	"@type.builtin",
+	"@variable",
+	"@variable.builtin",
+}
+
+for _, g in ipairs(ts_captures) do
+	groups[g] = { fg = fg }
+end
+
+for group, opts in pairs(groups) do
+	vim.api.nvim_set_hl(0, group, opts)
+end
--- .config/nvim/lua/krehwell/plugins/mason.lua
+++ .config/nvim/lua/krehwell/plugins/mason.lua
@@ -5,22 +5,34 @@ return {
 		{ "RRethy/vim-illuminate" },
 	},
 	ft = { "*" },
+	opts = {
+		ensure_installed = {
+			"biome",
+			"css_variables",
+			"cssls",
+			"cssmodules_ls",
+			"denols",
+			"gopls",
+			"html",
+			"jsonls",
+			"tsgo",
+		},
+	},
 	config = function(_, opts)
-		require("mason").setup({})
-		local lsp_utils = require("krehwell.lsp-utils")
+		require("mason").setup({ opts })
 
 		local servers = vim.iter(vim.api.nvim_get_runtime_file("lsp/*.lua", true))
 			:map(function(file)
 				return vim.fn.fnamemodify(file, ":t:r")
 			end)
 			:totable()
-
 		vim.lsp.enable(servers)
 
 		vim.api.nvim_create_autocmd("LspAttach", {
 			group = vim.api.nvim_create_augroup("krehwell/lsp_configure", { clear = true }),
 			desc = "LSP Setup",
 			callback = function(event)
+				local lsp_utils = require("krehwell.lsp-utils")
 				vim.diagnostic.config(lsp_utils.diagnostic_config)
 				lsp_utils.on_attach(event.buf)
 			end,
