628898c09e6281bc03b1594935a0cf6a200a22cd
krehwell <fahrimuham001@gmail.com>
2026-04-08 07:34:39 +0700

    dont run deno and tsgo lsp at the same time

--- .config/nvim/lsp/deno.lua
+++ .config/nvim/lsp/deno.lua
@@ -1,6 +1,7 @@
 return {
 	cmd = { "deno", "lsp" },
 	root_dir = vim.fs.root(0, { "deno.json" }), -- detect Deno project
+	filetypes = {},
 	init_options = {
 		lint = true, -- enable linting
 	},
--- .config/nvim/lsp/tsgo.lua
+++ .config/nvim/lsp/tsgo.lua
@@ -1,28 +1,16 @@
--- return {
--- 	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
--- 	cmd = { "tsgo", "--lsp", "--stdio" },
--- 	root_dir = vim.fs.root(0, { "package.json", "package.lock" }),
--- 	on_init = function(client)
--- 		client.server_capabilities.semanticTokensProvider = nil
--- 		client.server_capabilities.documentFormattingProvider = false
--- 		client.server_capabilities.documentRangeFormattingProvider = false
--- 		client.server_capabilities.codeLensProvider = nil
--- 		client.server_capabilities.documentHighlightProvider = false
--- 	end,
--- }
-
--- Install with: npm i -g @typescript/native-preview
-
----@type vim.lsp.Config
 return {
-	cmd = { "tsgo", "--lsp", "--stdio" },
 	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+	cmd = { "tsgo", "--lsp", "--stdio" },
 	root_dir = function(bufnr, on_dir)
 		local root_markers = { { "package-lock.json", "yarn.lock", "pnpm-lock.yaml" }, { ".git" } }
-
-		-- Fallback to the current working directory if no project root is found.
-		local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd()
-
+		local project_root = vim.fs.root(bufnr, root_markers) or vim.fn.getcwd() -- Fallback to the current working directory if no project root is found.
 		on_dir(project_root)
+	end,
+	on_init = function(client)
+		client.server_capabilities.semanticTokensProvider = nil
+		client.server_capabilities.documentFormattingProvider = false
+		client.server_capabilities.documentRangeFormattingProvider = false
+		client.server_capabilities.codeLensProvider = nil
+		client.server_capabilities.documentHighlightProvider = false
 	end,
 }
