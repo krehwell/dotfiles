7493081d59019f2ecc0a594ec0e9d5d209840a21
krehwell <fahrimuham001@gmail.com>
2026-05-21 01:26:46 +0700

    setup ts correctly

--- .config/nvim/lua/krehwell/plugins/treesitter.lua
+++ .config/nvim/lua/krehwell/plugins/treesitter.lua
@@ -1,7 +1,48 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    event = "BufReadPre",
     branch = "main",
-    -- enabled = false,
+    build = ":TSUpdate",
+    -- main branch does not lazy-load cleanly; load at startup so the
+    -- FileType autocmd below is registered before the first buffer opens.
+    lazy = false,
+    config = function()
+        local ts = require("nvim-treesitter")
+
+        -- async install; no-op if a parser is already present
+        ts.install({
+            "tsx",
+            "typescript",
+            "javascript",
+            "html",
+            "css",
+            "json",
+            "lua",
+            "vim",
+            "vimdoc",
+            "query",
+            "markdown",
+            "markdown_inline",
+        })
+
+        -- highlighting + experimental treesitter indentation per filetype
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = {
+                "tsx",
+                "typescript",
+                "typescriptreact",
+                "javascript",
+                "javascriptreact",
+                "html",
+                "css",
+                "json",
+                "lua",
+                "vim",
+                "markdown",
+            },
+            callback = function()
+                pcall(vim.treesitter.start)
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
+    end,
 }
