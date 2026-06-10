62efba50ae8d8345c65060d0c886f125621a7481
krehwell <fahrimuham001@gmail.com>
2026-06-11 00:46:20 +0700

    no antigravity

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -23,9 +23,3 @@ if status is-interactive
 end
 
 export PATH="$HOME/.local/bin:$PATH"
-
-# Added by Antigravity
-fish_add_path /Users/ard/.antigravity/antigravity/bin
-
-# Added by Antigravity IDE
-fish_add_path /Users/ard/.antigravity-ide/antigravity-ide/bin
