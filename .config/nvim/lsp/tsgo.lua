cb7742e59149b8d3f117ea99aa1ce086b178fc6c
krehwell <fahrimuham001@gmail.com>
2026-06-15 01:14:17 +0700

    from vps

--- .config/nvim/lsp/tsgo.lua
+++ .config/nvim/lsp/tsgo.lua
@@ -1,6 +1,6 @@
 ---@type vim.lsp.Config
 return {
-    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+    filetypes = {},
     cmd = { "tsgo", "--lsp", "--stdio" },
     root_dir = function(bufnr, on_dir)
         local fname = vim.api.nvim_buf_get_name(bufnr)
