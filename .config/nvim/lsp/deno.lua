f8a6bd86cf57effed44749e08756cc1d9e698f48
krehwell <fahrimuham001@gmail.com>
2026-06-15 01:24:28 +0700

    deno and tsgo won't conflict anymore

--- .config/nvim/lsp/deno.lua
+++ .config/nvim/lsp/deno.lua
@@ -2,7 +2,14 @@
 return {
 	cmd = { "deno", "lsp" },
     filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-	root_markers = { "deno.json", "deno.jsonc" },
+	root_dir = function(bufnr, on_dir)
+		local fname = vim.api.nvim_buf_get_name(bufnr)
+		local deno_root = vim.fs.root(fname, { "deno.json", "deno.jsonc" })
+		-- Only attach in actual Deno projects; otherwise stay off so tsgo handles it.
+		if deno_root then
+			on_dir(deno_root)
+		end
+	end,
 	init_options = {
 		lint = true,
 	},
--- .config/nvim/lsp/tsgo.lua
+++ .config/nvim/lsp/tsgo.lua
@@ -1,6 +1,6 @@
 ---@type vim.lsp.Config
 return {
-    filetypes = {},
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
     cmd = { "tsgo", "--lsp", "--stdio" },
     root_dir = function(bufnr, on_dir)
         local fname = vim.api.nvim_buf_get_name(bufnr)
